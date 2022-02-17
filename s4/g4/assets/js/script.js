5c78bf27d0e5ca08265b4a23497ab8580b93a343
EMilanopulo <milanopulo@gmail.com>
2022-02-17 18:05:02 +0100

    esercizio s4g4

---
+++ s4/g4/assets/js/script.js
@@ -0,0 +1,30 @@
+let titolo = document.getElementById('titolo').innerHTML = "Esercitazioni con i metodi dell\' Array";
+
+document.getElementById('titolo').innerHTML= (titolo.toUpperCase());
+
+document.getElementById('nunzio').innerHTML = (titolo.substring(17));
+
+var persone = [
+    "Elisabetta",
+    "Erica",
+    "Federico",
+    "Sergiu"
+]
+
+document.getElementById('selez').innerHTML= persone[2];
+
+let variabilePush = persone.push('Nunzio');
+    document.getElementById('aggNome').innerHTML = persone;
+
+    console.log(persone.length);
+
+var numeri  = [
+    32,
+    46,
+    9,
+    3
+]
+
+function oper() {
+    document.getElementById('operazione').innerHTML= numeri[1] * numeri[3];
+}
