22198a81af5910cb6a4fc1edb94082cde478c49c
EMilanopulo <milanopulo@gmail.com>
2022-03-15 16:54:20 +0100

    esercizio s8g2

---
+++ s8/g2/typescript/demo.js
@@ -0,0 +1,63 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//SUPERCLASSE
+var SonAccount = /** @class */ (function () {
+    function SonAccount(balanceInit) {
+        this.versamenti = []; //soldi che aggiungiamo
+        this.prelievi = []; //soldi che tolgo
+        this.balanceInit = balanceInit;
+        //this.versamenti = versamenti;
+        //this.prelievi = prelievi;
+    }
+    SonAccount.prototype.oneDeposit = function (num) {
+        this.versamenti.push(num);
+        return this.balanceInit += num;
+    };
+    SonAccount.prototype.oneWithDraw = function (num) {
+        this.prelievi.push(num);
+        return this.balanceInit -= num;
+    };
+    return SonAccount;
+}());
+//SOTTOCLASSE
+var MotherAccount = /** @class */ (function (_super) {
+    __extends(MotherAccount, _super);
+    function MotherAccount(balanceInit) {
+        return _super.call(this, balanceInit) || this; //versamenti, prelievi)
+    }
+    MotherAccount.prototype.interests = function () {
+        return (this.balanceInit / 100) * 10;
+    };
+    MotherAccount.prototype.addInterest = function () {
+        return this.balanceInit += this.interests();
+    };
+    return MotherAccount;
+}(SonAccount));
+//oggetti
+var madre = new MotherAccount(1000);
+var figlio = new SonAccount(2000);
+//output
+console.log('Versamento nel conto del figlio: ' + figlio.oneDeposit(100));
+console.log('Saldo aggiornato dopo versamento: ' + figlio.balanceInit);
+console.log('Aggiunta interesse conto madre: ' + madre.addInterest());
+console.log(madre.balanceInit);
+console.log(madre.addInterest());
+console.log(madre.oneWithDraw(200));
+console.log(madre.addInterest());
+console.log(madre.oneDeposit(1800));
+console.log(madre.oneDeposit(800));
+console.log(madre.balanceInit);
+console.log(madre.addInterest());
---
+++ s8/g2/typescript/demo.ts
@@ -0,0 +1,70 @@
+//SUPERCLASSE
+class SonAccount {
+
+    balanceInit: number; //saldo attuale
+    versamenti: number[]=[]; //soldi che aggiungiamo
+    prelievi: number[]=[]; //soldi che tolgo
+
+    constructor(balanceInit: number) {
+
+        this.balanceInit = balanceInit;
+        //this.versamenti = versamenti;
+        //this.prelievi = prelievi;
+
+    }
+
+    oneDeposit(num:number):number{
+        this.versamenti.push(num)
+        return this.balanceInit += num;
+    }
+
+    oneWithDraw(num:number):number{
+        this.prelievi.push(num)
+        return this.balanceInit -= num
+    }
+
+}
+
+//SOTTOCLASSE
+class MotherAccount extends SonAccount {
+
+    constructor(balanceInit: number) //versamenti: number[], prelievi: number[],) 
+   {
+        super(balanceInit) //versamenti, prelievi)
+
+    }
+
+    interests(){
+        return (this.balanceInit/100)*10
+    }
+
+    addInterest():number{
+
+    return this.balanceInit += this.interests();
+
+    }
+    
+}
+
+//oggetti
+let madre = new MotherAccount(1000);
+let figlio = new SonAccount(2000);
+
+//output
+console.log('Versamento nel conto del figlio: ' + figlio.oneDeposit(100));
+console.log('Saldo aggiornato dopo versamento: ' + figlio.balanceInit);
+
+console.log('Aggiunta interesse conto madre: ' + madre.addInterest());
+console.log(madre.balanceInit);
+console.log(madre.addInterest());
+console.log(madre.oneWithDraw(200));
+console.log(madre.addInterest());
+console.log(madre.oneDeposit(1800));
+console.log(madre.oneDeposit(800));
+console.log(madre.balanceInit);
+console.log(madre.addInterest());
+
+
+
+
+
